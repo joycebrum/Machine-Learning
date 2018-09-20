0e550b57f5d4205d56a12b4c60aa70e7870fd776
Thiago <topd97@yahoo.com.br>
2018-09-20 16:10:40 -0300

    comparação sendo feita
    17 acertos e 13 erros

--- machinelearning.py
+++ machinelearning.py
@@ -31,12 +31,13 @@ def classification() :
     j=0
     for linha in texto :
         x.append([])
+        x[j].append([])
         element = linha.split(',')
         pos = len(element)-1
         classify = element[pos];
+        classify=classify[0:len(classify)-1]        
         element.pop(pos)
         if classify == "Iris-setosa" :
-            p
             classify = 1
         elif classify == "Iris-versicolor" :
             classify = -1
@@ -44,28 +45,54 @@ def classification() :
             classify = -1
         for i in range(0,pos) :
             element[i] = float(element[i])
-        x[j].append(element)
+        x[j][0].append(element)
         x[j].append(classify)
         j=j+1
     w = [[1],[1],[1],[1]]
     i=0
-    wt=np.transpose(w)
-    xt=np.transpose(x)
-    
-    print(wt)
-    print(x[0][1])
-    print(np.transpose(x[0][0]))
     for i in range(0, len(x)):
-        res=np.dot(wt,np.transpose(x[i][0]))
         
-        print("resultado ", i, res)
-        print(np.sign(res[0]))
-        print(x[i][1])
+        res=np.dot(np.transpose(w),np.transpose(x[i][0]))
+        
+        if(np.sign(res[0]) != x[i][1]):
+            w=w+x[i][1]*np.transpose(x[i][0])
+    print("resultado:", w)
+
+
+
+    arq2 = open('irisDataTest.txt', 'r')
+    x = []
+    texto = arq2.readlines()
+    arq2.close()
+    j=0
+    for linha in texto :
+        x.append([])
+        x[j].append([])
+        element = linha.split(',')
+        pos = len(element)-1
+        classify = element[pos];
+        classify=classify[0:len(classify)-1]        
+        element.pop(pos)
+        if classify == "Iris-setosa" :
+            classify = 1
+        elif classify == "Iris-versicolor" :
+            classify = -1
+        else :
+            classify = -1
+        for i in range(0,pos) :
+            element[i] = float(element[i])
+        x[j][0].append(element)
+        x[j].append(classify)
+        j=j+1
+    i=0
+    for i in range(0, len(x)):
+        
+        res=np.dot(np.transpose(w),np.transpose(x[i][0]))
+        
         if(np.sign(res[0]) == x[i][1]):
-            print("YYYEEEEAAAAHHH")
-
-
-
+            print("correto")
+        else:
+            print("ERRROOOUUU")
 
 
 
